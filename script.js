fb344ee58a3519be9ea18ec5ed86ef1c8c8e8793
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-06-01 10:46:07 +0100

    added js

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
 function getData(){
     header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiojEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="">'
     title.innerHTML = 'Lorem ipsum dolor sit amet.'
-
+    excerpt.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, voluptates!'
+    profile_img.innerHTML = ''
 }
 
 
