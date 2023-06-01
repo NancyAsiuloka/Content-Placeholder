aa75f3d32ea5e6764159295fdc599e3eefda7d2b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-06-01 10:44:02 +0100

    added js

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const date = document.getElementById('date');
 
 const animated_bgs = document.querySelectorAll('.animated-bg');
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
+
+function getData(){
+    header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiojEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="">'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+
+}
+
+
